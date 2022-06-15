c5817597ee289bcc76bf520c13cf4d570b65deca
SalmanSaalu <salmanpp05@gmail.com>
2022-06-15 23:51:34 +0530

    not fully completed upto cred

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Firebase } from './firebase/config';
+import {getFirestore,collection,getDocs,addDoc,deleteDoc, doc, setDoc } from 'firebase/firestore/lite'
 
+function readdata(){
+  const w=getFirestore(Firebase)
+  const u=collection(w,'products')
+  getDocs(u).then(doc=>doc.docs.forEach(r=>console.log(r.data(),r.id)))
+}
+
+
+function adds(){
+  const w=getFirestore(Firebase)
+  const u=collection(w,'products')
+  addDoc(u,{
+    name:'MI',
+    price:1200
+  })
+  //u can also use setDoc instead of set doc
+  
+}
+
+
+function del(){
+  const w=getFirestore(Firebase)
+  const u=collection(w,'products')
+  deleteDoc(doc(u,'Rc9BIx9Su0rBMhyNU'))
+  // const u=collection(w,'products').doc('')
+  // // const s=getDocs(u)
+  // deleteDoc(u)
+//   deleteDoc(u,'Jr8PYRgeBT7nMxVRpwJr')
+}
+
+function upd(){
+  const w=getFirestore(Firebase)
+  const u=collection(w,'products')
+  setDoc(doc(u,'Rc9BIx9Su0rBMhyNUk8C'),{
+    name:"updated"
+  })
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={readdata}>click me</button>
+    <button onClick={adds}>add</button>
+    <button onClick={del}>delete</button>
+   < button onClick={upd}>update</button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app'
+// import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyBeBwshQXhDB583N_26yHXF5QO38yZwqxc",
+    authDomain: "fir-2d75e.firebaseapp.com",
+    projectId: "fir-2d75e",
+    storageBucket: "fir-2d75e.appspot.com",
+    messagingSenderId: "1004443351529",
+    appId: "1:1004443351529:web:26169e843716cadb5b7378",
+    measurementId: "G-MDXPKG86VM"
+  };
+
+export const Firebase=initializeApp(firebaseConfig)
+
+
